fbcb3de1f98f448d4063cb16fb690c976a09422e
Zeya Mosharraf <119008382+zeyamosharraf@users.noreply.github.com>
2025-03-02 12:52:14 +0530

    Update script.js

--- portfolio-test/script.js
+++ portfolio-test/script.js
@@ -1,3 +1,48 @@
-document.getElementById("clickMe").addEventListener("click", function() {
-    alert("Button Clicked!");
+/* script.js */
+
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+// Typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'UI/UX Designer', 'App Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
